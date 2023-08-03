00e12b12bfffd531841c942f67c3206fbb682665
Pharanya <pharanya_li@cmu.ac.th>
2023-08-03 15:30:18 +0700

    4.6

--- real-world-vue/src/components/EventCard.vue
+++ real-world-vue/src/components/EventCard.vue
@@ -1,37 +1,36 @@
 <template>
-    <div class="event-class">
-        <span>@{{ event.time }} on {{ event.date }}</span>
-        <h4>{{ event.title }}</h4>
-    </div>
+  <div class="event-class">
+    <span>@{{ event.time }} on {{ event.date }}</span>
+    <h4>{{ event.title }}</h4>
+  </div>
 </template>
 
 <script setup lang="ts">
-import {ref} from 'vue'
+import { ref } from 'vue'
 const event = ref({
-    id: 5928101,
-    category: 'animal welfare',
-    title: 'Cat Adoption Day',
-    description: 'Find your new feline friend at this event.',
-    location: 'Meow Town',
-    date: 'Jauary 28, 2022',
-    time: '12:00',
-    petsAllowed: true,
-    organizer: 'Key Laydee'
+  id: 5928101,
+  category: 'animal welfare',
+  title: 'Cat Adoption Day',
+  description: 'Find your new feline friend at this event.',
+  location: 'Meow Town',
+  date: 'Jauary 28, 2022',
+  time: '12:00',
+  petsAllowed: true,
+  organizer: 'Key Laydee'
 })
-
 </script>
 
 <style scoped>
 .event-card {
-    padding: 20px;
-    widows: 250px;
-    cursor: pointer;
-    border: 1px solid #39495c;
-    margin-bottom: 18px;
+  padding: 20px;
+  widows: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
 }
 
 .event-card:hover {
-    transform: scale(1.01);
-    box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
 }
 </style>
